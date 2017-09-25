8ea29dcc214d20a45e65a9b9d419d829bb689872
juane <juane_619@hotmail.com>
2017-09-25 15:00:47 +0200

    first

--- app/src/main/java/com/example/android/miwok/MainActivity.java
+++ app/src/main/java/com/example/android/miwok/MainActivity.java
@@ -32,7 +32,7 @@ public class MainActivity extends AppCompatActivity {
 
         NumbersClickListener clickListener = new NumbersClickListener();
         TextView numbersView= (TextView) findViewById(R.id.numbers);
-        numbersView.setOnClickListener(clickListener);
+        numbersView.setOnClickListener(prueba);
     }
 
 //    public void openNumbersList(View view) {
